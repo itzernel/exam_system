0ca6a795318700143b41e9d336d36f846afeb5b7
Jeweller-Tsai <jiangnan34@gmail.com>
2011-09-14 22:33:05 +0800

    provide html format

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,11 +1,17 @@
 class PapersController < ApplicationController
   def index
-    papers = Paper.all
-    render_for_api :paper, :json => papers, :status => :ok, :root => :papers
+    @papers = Paper.all
+    respond_to do |format|
+      format.html
+      format.json { render_for_api :paper, :json => @papers, :status => :ok, :root => :papers }
+    end
   end
 
   def show
-    paper = Paper.find(params[:id])
-    render_for_api :paper_with_questions, :json => paper, :status => :ok
+    @paper = Paper.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render_for_api :paper_with_questions, :json => @paper, :status => :ok }
+    end
   end
 end
